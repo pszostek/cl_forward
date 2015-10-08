85e2f5a52de2a300f4090d75e525ef84f8ea6332
Pawel Szostek <pawel.szostek@cern.ch>
2015-10-08 15:09:11 +0200

    Revert "change most of the macros to const global variables"
    This reverts commit 3eebd382e70c1cd911945744be36fadbf95f4bef.

--- KernelDefinitions.h
+++ KernelDefinitions.h
@@ -1,6 +1,4 @@
 
-#ifndef KERNEL_DEFINITIONS_H
-#define KERNEL_DEFINITIONS_H
 // Used to prefer a device type over another one
 #define DEVICE_CPU 0
 #define DEVICE_GPU 1
@@ -8,32 +6,32 @@
 #define DEVICE_PREFERENCE DEVICE_GPU
 #define DEVICE_NUMBER 0
 
-const unsigned int NUMTHREADS_X = 64;
-const unsigned int MAX_NUMTHREADS_Y = 16;
-const unsigned int NUM_ATOMICS = 5;
+#define NUMTHREADS_X 64
+#define MAX_NUMTHREADS_Y 16
+#define NUM_ATOMICS 5
 #define USE_SHARED_FOR_HITS false
-const unsigned int SH_HIT_MULT = 2;
+#define SH_HIT_MULT 2
 
-const unsigned int MAX_TRACKS = 3000;
-const unsigned int MAX_TRACK_SIZE = 24;
+#define MAX_TRACKS 3000
+#define MAX_TRACK_SIZE 24
 
-const double REQUIRED_UNIQUES = 0.6f;
-const unsigned int MIN_HITS_TRACK = 3;
-const float MAX_FLOAT = FLT_MAX;
-const float MIN_FLOAT = -FLT_MAX;
-const unsigned int MAX_SKIPPED_MODULES = 3;
-const unsigned int TTF_MODULO = 2000;
+#define REQUIRED_UNIQUES 0.6f
+#define MIN_HITS_TRACK 3
+#define MAX_FLOAT FLT_MAX
+#define MIN_FLOAT -FLT_MAX
+#define MAX_SKIPPED_MODULES 3
+#define TTF_MODULO 2000
 
-const double PARAM_W = 3966.94f; // 0.050 / sqrt( 12. )
-const double PARAM_MAXXSLOPE = 0.4f;
-const double PARAM_MAXYSLOPE = 0.3f;
-const double PARAM_MAXXSLOPE_CANDIDATES = 0.4f;
+#define PARAM_W 3966.94f // 0.050 / sqrt( 12. )
+#define PARAM_MAXXSLOPE 0.4f
+#define PARAM_MAXYSLOPE 0.3f
+#define PARAM_MAXXSLOPE_CANDIDATES 0.4f
 
-const double PARAM_TOLERANCE = 0.6f;
-const double PARAM_TOLERANCE_CANDIDATES = 0.6f;
+#define PARAM_TOLERANCE 0.6f
+#define PARAM_TOLERANCE_CANDIDATES 0.6f
 
-const double MAX_SCATTER = 0.000016f;
-const unsigned int SENSOR_DATA_HITNUMS = 3;
+#define MAX_SCATTER 0.000016f
+#define SENSOR_DATA_HITNUMS 3
 #define RESULTS_FOLDER "results"
 
 #define PRINT_SOLUTION true
@@ -62,5 +60,3 @@ struct Track { // 4 + 24 * 4 = 100 B
     unsigned int hitsNum;
     unsigned int hits[MAX_TRACK_SIZE];
 };
-
-#endif
