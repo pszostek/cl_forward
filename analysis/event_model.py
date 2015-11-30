92f9f1b8ce349f02f45cf33d964236e95f42a3bc
Omar Awile <omar.awile@cern.ch>
2015-11-30 10:43:20 +0100

    Factored out event model code into seperate module
    the new event_model module provides classes and parser functions for processing
    data files used in testing tracking algorithms (simplified event/track data
    files).

---
+++ analysis/event_model.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+from struct import unpack
+
+class Event(object):
+    """A SOA datastructure for events"""
+
+    def __init__(self, sensor_Zs, sensor_hitStarts, sensor_hitNums,
+        hit_IDs, hit_Xs, hit_Ys, hit_Zs, mcp_to_hits=None):
+        self.sensor_Zs = sensor_Zs
+        self.sensor_hitStarts = sensor_hitStarts
+        self.sensor_hitNums = sensor_hitNums
+        self.hit_IDs = hit_IDs
+        self.hit_Xs, self.hit_Ys, self.hit_Zs = hit_Xs, hit_Ys, hit_Zs
+        self.mcp_to_hits = mcp_to_hits
+        self.hit_to_mcp = {h:[] for h in self.hit_IDs}
+        for mcp,mhits in mcp_to_hits.iteritems():
+            for hit in mhits:
+                self.hit_to_mcp[hit].append(mcp)
+
+
+
+class MCParticle(object):
+    """Store information about a Monte-Carlo simulation particle"""
+
+    def __init__(self, pkey, pid, velohits):
+        """Construct a new particle from
+
+        its numeric key (arbitrary integer used in input file)
+        its pID (integer providing information on the particle type)
+        its assocated velopixel hits"""
+        self.pkey = pkey
+        self.pid = pid
+        self.velohits = velohits
+
+    def __str__(self):
+        return "MCParticle %d: pid = %d"%(self.pkey, self.pid)
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.__dict__)
+
+
+class Hit(object):
+    """Hit instances store all relevant information of hits from the track"""
+
+    def __init__(self, hitID, x=0.0, y=0.0, z=0.0, module=0):
+        """Construct a hit from its coordinates and ID"""
+        self.hitID = hitID
+        self.module = module
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return "%d M#(%d): x = %g, y = %g, z = %g"%(self.hitID, self.module, self.x, self.y, self.z)
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.__dict__)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return self.hitID
+
+
+class Track(object):
+    """Track instances hold all information needed to reconstruct a track, that
+    includes also all the hits that make up this track"""
+
+    def __init__(self, tid, trackhits):
+        """Constructs a new track from a list of hits"""
+        self.tid = tid
+        self.hits = trackhits
+
+    def get_coords(self):
+        """Return three lists for x, y and z coordinates of hits.
+        This is especially useful for using with matplotlib 3D plotting."""
+        xs = []
+        ys = []
+        zs = []
+        for hit in self.hits:
+            xs.append(hit.x)
+            ys.append(hit.y)
+            zs.append(hit.z)
+        return (xs, ys, zs)
+
+    def __str__(self):
+        s = 'Track (#%d) length: %d'%(self.tid, len(self.hits))
+        for hit in self.hits:
+            s += '\n' + str(hit)
+        return s
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.__dict__)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if len(self.hits) != len(other.hits):
+                return False
+            eq = True
+            for i, h in enumerate(self.hits):
+                eq = eq and (h == other.hits[i])
+            return eq
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        hsh = 0
+        for i,h in enumerate(self.hits):
+            hsh += (i+1)*h.hitID
+        return hsh
+
+def read_datfile(filename):
+    with open(filename, 'rb') as f:
+        bindata = f.read()
+    pos = 0
+    # read file header
+    funcNameLen, = unpack('i', bindata[pos:pos+4])
+    pos += 4
+    funcName = unpack('c'*funcNameLen, bindata[pos:pos+funcNameLen])
+    funcName = ''.join(funcName)
+    pos += funcNameLen
+    dataLen, = unpack('i', bindata[pos:pos+4])
+    pos += 4
+    header_sz = pos
+    no_sensors, no_hits = unpack('ii', bindata[pos:pos+8])
+    pos += 8
+    # read sensor data
+    sensor_Zs = unpack('i'*no_sensors, bindata[pos:pos+no_sensors*4])
+    pos += no_sensors*4
+    sensor_hitStarts = unpack('i'*no_sensors, bindata[pos:pos+no_sensors*4])
+    pos += no_sensors*4
+    sensor_hitNums = unpack('i'*no_sensors, bindata[pos:pos+no_sensors*4])
+    pos += no_sensors*4
+    # read hit data
+    hit_IDs = unpack('i'*no_hits, bindata[pos:pos+no_hits*4])
+    pos += no_hits*4
+    hit_Xs = unpack('f'*no_hits, bindata[pos:pos+no_hits*4])
+    pos += no_hits*4
+    hit_Ys = unpack('f'*no_hits, bindata[pos:pos+no_hits*4])
+    pos += no_hits*4
+    hit_Zs = unpack('f'*no_hits, bindata[pos:pos+no_hits*4])
+    pos += no_hits*4
+    hits = []
+    for hid, x, y, z in zip(hit_IDs, hit_Xs, hit_Ys, hit_Zs):
+        hits.append(Hit(hid, x, y, z))
+    mcp_to_hits = {}
+    if (pos < dataLen + header_sz):
+        # it looks like we have some MCP data in this file, proceed
+        no_mcp, = unpack('i', bindata[pos:pos+4])
+        pos += 4
+        for _ in range(no_mcp):
+            mcpid, nh = unpack('ii', bindata[pos:pos+8])
+            pos += 8
+            mcp_hitIDs = unpack('i'*nh, bindata[pos:pos+nh*4])
+            pos += nh*4
+            hdict = {h.hitID:h for h in hits}
+            trackhits = [hdict[hid] for hid in mcp_hitIDs]
+            mcp_to_hits[mcpid] = trackhits
+    return Event(sensor_Zs, sensor_hitStarts, sensor_hitNums,
+            hit_IDs, hit_Xs, hit_Ys, hit_Zs, mcp_to_hits), hits
+
+def read_bin_trackfile(filename, hits=None):
+    with open(filename,'rb') as f:
+        bindata = f.read()
+    pos = 0
+    nTracks, = unpack('i',bindata[pos:pos+4])
+    pos += 4
+    tracks = set()
+    for itrack in range(nTracks):
+        nhits, = unpack('i', bindata[pos:pos+4])
+        pos += 4
+        track_hitIDs = unpack('i'*nhits, bindata[pos:pos+nhits*4])
+        if hits is None:
+            trackhits = [Hit(hid) for hid in track_hitIDs]
+        else:
+            hdict = {h.hitID:h for h in hits}
+            trackhits = [hdict[hid] for hid in track_hitIDs]
+        pos += nhits*4
+        tracks.add(Track(itrack, trackhits))
+    return tracks
+
+
+def read_txt_trackfile(filename):
+    """A simple cl_forward output file parser.
+
+    returns a set of tracks in the file
+    """
+    tracks = set()
+    with open(filename) as tf:
+        line = tf.readline()
+        while line:
+            if line.startswith('Track'):
+                tid = int(line.strip().split()[1][1:-1])
+                tlen = int(line.strip().split()[-1])
+                trackhits = []
+                for _ in xrange(tlen):
+                    hitline = tf.readline()
+                    flds = hitline.strip().split()
+                    hitID, module = int(flds[0]),int(flds[3][:-1])
+                    #self.hitNum = int(flds[1][1:-1])
+                    x, y, z = float(flds[5][:-1]), float(flds[7][:-1]), float(flds[9])
+                    trackhits.append(Hit(hitID, x, y, z, module))
+                track = Track(tid,trackhits)
+                tracks.add(track)
+            line = tf.readline()
+    return tracks
--- analysis/trackreader.py
+++ analysis/trackreader.py
@@ -3,112 +3,9 @@
 import argparse
 import errno
 import os
+import event_model
 
 
-class Hit(object):
-    """Hit instances store all relevant information of hits from the track"""
-
-    def __init__(self, hitline):
-        """Reads a hit line from the cl_forward output file and creates a Hit object"""
-        fields = hitline.strip().split()
-        self.hitID = int(fields[0])
-        self.hitNum = int(fields[1][1:-1])
-        self.module = int(fields[3][:-1])
-        self.x = float(fields[5][:-1])
-        self.y = float(fields[7][:-1])
-        self.z = float(fields[9])
-
-    def __str__(self):
-        return "%d (%d) M#(%d): x = %g, y = %g, z = %g"%(self.hitID, self.hitNum, self.module, self.x, self.y, self.z)
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__class__, self.__dict__)
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        return self.hitID
-
-
-class Track(object):
-    """Track instances hold all information needed to reconstruct a track, that
-    includes also all the hits that make up this track"""
-
-    def __init__(self, tid, lines):
-        """Takes a section of cl_forward output file that contains all the track hits"""
-        self.tid = tid
-        self.hits = []
-        for line in lines:
-            hit = Hit(line)
-            self.hits.append(hit)
-
-    def get_coords(self):
-        """Return three lists for x, y and z coordinates of hits.
-        This is especially useful for using with matplotlib 3D plotting."""
-        xs = []
-        ys = []
-        zs = []
-        for hit in self.hits:
-            xs.append(hit.x)
-            ys.append(hit.y)
-            zs.append(hit.z)
-        return (xs, ys, zs)
-
-    def __str__(self):
-        s = 'Track (#%d) length: %d'%(self.tid,len(self.hits))
-        for hit in self.hits:
-            s += '\n' + str(hit)
-        return s
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__class__, self.__dict__)
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            if len(self.hits) != len(other.hits):
-                return False
-            eq = True
-            for i, h in enumerate(self.hits):
-                eq = eq and (h == other.hits[i])
-            return eq
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self):
-        hsh = 0
-        for i,h in enumerate(self.hits):
-            hsh += (i+1)*h.hitID
-        return hsh
-
-def read_trackfile(filename):
-    """A simple cl_forward output file parser.
-
-    returns a set of tracks in the file
-    """
-    tracks = set()
-    with open(filename) as tf:
-        line = tf.readline()
-        while line:
-            if line.startswith('Track'):
-                tid = int(line.strip().split()[1][1:-1])
-                tlen = int(line.strip().split()[-1])
-                tracklines = []
-                for _ in xrange(tlen):
-                    tracklines.append(tf.readline())
-                track = Track(tid,tracklines)
-                tracks.add(track)
-            line = tf.readline()
-    return tracks
 
 
 def main():
@@ -129,9 +26,9 @@ def main():
             print("File doesn't exist: %s." % file_)
             exit(errno.ENOENT)
 
-    tracks1 = read_trackfile(args.files[0])
+    tracks1 = event_model.read_txt_trackfile(args.files[0])
     try:
-        tracks2 = read_trackfile(args.files[1])
+        tracks2 = event_model.read_txt_trackfile(args.files[1])
     except IndexError:  # it will be raised if there is only one file
         tracks2 = None
 
